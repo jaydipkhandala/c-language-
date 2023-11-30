cb8c8462feebf3436fbee125a7443c292c994a64
jaydipkhandala <152478337+jaydipkhandala@users.noreply.github.com>
2023-11-30 13:28:52 +0530

    addition

---
+++ JAYDIP.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+	clrscr();
+	printf("NAME : khandala jaydip\n");
+	printf("GR ID : 6308\n");
+	printf("COURSE : full stack\n");
+	printf("BRANCH : RWA2\n");
+	printf("MO :7874643462\n");
+	printf("ADDRESS : AT-vasna , TA - dhandhuka\n");
+	printf("E-MAIL : khandalajaydip@761.gmail.com \n");
+	getch();
+}
